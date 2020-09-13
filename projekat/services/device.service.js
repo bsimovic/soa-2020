3b073f91606f566a2c1de29db6b85053b1f7aac7
bsimovic <b3jn@outlook.com>
2020-09-13 17:43:23 +0200

    Snalazim se nekako...

---
+++ projekat/services/device.service.js
@@ -0,0 +1,31 @@
+"use strict"
+
+module.exports = {
+    name: "device",
+    methods: {
+        init() {
+            this.count = 0;
+            
+        },
+
+        asd() {
+
+        }
+    },
+    events: {
+        "change.source": {
+
+        },
+
+        "change.unit": {
+
+        },
+
+        "change.interval": {
+
+        }
+    }
+
+
+
+}
